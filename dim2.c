295abba4037418d032dc4e5ff0ebee35ebd46bc2
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2025-02-07 09:40:23 +0530

    Add files via upload

---
+++ dim2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int row,col,sum=0;
+    printf("\n Enter the row and column:");
+    scanf("%d%d",&row,&col);
+    int matrix[row][col];
+    printf("\n Enter the elements:");
+    for(int i=0;i<row;i++){
+       for(int j=0;j<col;j++){
+          scanf("%d",&matrix[i][j]);
+          }
+    }
+    for(int i=0;i<row;i++){
+       for(int j=0;j<col;j++){
+          if(i==j){
+            sum=sum+matrix[i][j];
+           } 
+        }
+     }   
+     for(int i=0;i<row;i++){
+       for(int j=0;j<col;j++){
+          printf("%d\t",matrix[i][j]);
+          }
+        printf("\n");
+        }
+     printf("\n The sum is:%d",sum);
+     return 0;
+     }
